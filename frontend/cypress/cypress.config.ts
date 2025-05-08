150fcb87f2e388b98987ba7ff0c9473eda8cf2a8
anasseljazouly <144116191+a-eljazouly@users.noreply.github.com>
2025-05-08 14:37:08 +0100

    config reports

--- frontend/cypress/cypress.config.ts
+++ frontend/cypress/cypress.config.ts
@@ -3,16 +3,12 @@ import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
 import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
 import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild';
 
-const reporter = await import('cypress-multi-reporters').then(
-  (mod) => mod.default
-);
-
 export default defineConfig({
   chromeWebSecurity: false,
   defaultCommandTimeout: 6000,
   e2e: {
     excludeSpecPattern: ['*.js', '*.ts', '*.md'],
-    reporter,
+    reporter: require.resolve('cypress-multi-reporters'),
     reporterOptions: {
       configFile: `reporter-config.js`
     },
@@ -26,15 +22,15 @@ export default defineConfig({
       );
       return config;
     },
-    fixturesFolder: 'cypress/e2e/fixtures',
-    supportFile: 'cypress/e2e/support/e2e.ts',
-    specPattern: 'cypress/e2e/features/**/*.feature'
+    fixturesFolder: 'e2e/fixtures',
+    supportFile: 'e2e/support/e2e.ts',
+    specPattern: 'e2e/features/**/*.feature'
   },
   execTimeout: 10000,
   requestTimeout: 10000,
   retries: 0,
-  screenshotsFolder: `cypress/results/screenshots`,
+  screenshotsFolder: `e2e/results/screenshots`,
   video: true,
   videoCompression: 0,
-  videosFolder: `cypress/results/videos`
+  videosFolder: `e2e/results/videos`
 });
--- frontend/cypress/reporter-config.js
+++ frontend/cypress/reporter-config.js
@@ -5,7 +5,7 @@ module.exports = {
     html: false,
     json: true,
     overwrite: true,
-    reportDir: 'results/reports',
+    reportDir: 'e2e/results/reports',
     reportFilename: '[name]-report.json'
   },
   reporterEnabled: `mochawesome,${require.resolve(
